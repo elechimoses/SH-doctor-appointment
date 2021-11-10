c4fe9bef66aa94fd4df3fd59d317c9d04e6dff88
Adeyinka Adefolurin <folurinyinka@gmail.com>
2021-11-10 07:17:36 +0100

    write seeder for users
    this will seed a random users + roles for the users - doctors, patients, admins.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = collect(['admin', 'doctor', 'patient']);
+
+        $users = User::factory()
+            ->count(10)
+            ->state(new Sequence(
+                fn () => ['role' => $roles->random()],
+            ))
+            ->create();
+    }
+}
